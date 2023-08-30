a4e25d932cdb253425fc00a25bc13d9137f7259d
Rafael <rafaelrlcnta@gmail.comm>
2023-08-29 23:38:09 -0300

    feat: retorno de Http conflict quando houve conflito de horarios no save do agendamento

--- back-end/agendadorConsulta/src/main/java/br/com/rang/agendadorConsulta/controller/AgendamentoController.java
+++ back-end/agendadorConsulta/src/main/java/br/com/rang/agendadorConsulta/controller/AgendamentoController.java
@@ -1,6 +1,9 @@
 package br.com.rang.agendadorConsulta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,7 @@ import br.com.rang.agendadorConsulta.crud.CrudController;
 import br.com.rang.agendadorConsulta.crud.CrudService;
 import br.com.rang.agendadorConsulta.model.Agendamento;
 import br.com.rang.agendadorConsulta.service.AgendamentoService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/agendamento")
@@ -15,10 +19,19 @@ public class AgendamentoController extends CrudController<Agendamento, Long> {
 
 	@Autowired
 	private AgendamentoService agendamentoService;
-	
+
 	@Override
 	public CrudService<Agendamento, Long> getService() {
 		return agendamentoService;
 	}
-	
+
+	@Override
+	public ResponseEntity<?> save(@Valid @RequestBody Agendamento entity) {
+		try {
+			return super.save(entity);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+		}
+	}
+
 }
