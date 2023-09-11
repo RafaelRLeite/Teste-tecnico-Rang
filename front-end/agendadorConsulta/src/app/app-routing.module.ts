0d1cb622f25c282202504c75b4d2ef832c89eaba
Rafael <rafaelrlcnta@gmail.comm>
2023-09-11 00:21:35 -0300

    feat: criação das rotas, inclusão de novas bibliotecas

--- front-end/agendadorConsulta/src/app/app-routing.module.ts
+++ front-end/agendadorConsulta/src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarAgendamentoComponent } from './componentes/agendamentos/criar-agendamento/criar-agendamento.component';
+import { ListarAgendamentosComponent } from './componentes/agendamentos/listar-agendamentos/listar-agendamentos.component';
 
 const routes: Routes = [
-  {path: '', component: CriarAgendamentoComponent}
+  { path: '' ,
+    redirectTo: 'criarAgendamento',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criarAgendamento',
+    component: CriarAgendamentoComponent
+  },
+  {
+    path:'listarAgendamento',
+    component: ListarAgendamentosComponent
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- front-end/agendadorConsulta/src/app/app.module.ts
+++ front-end/agendadorConsulta/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
 import { CriarAgendamentoComponent } from './componentes/agendamentos/criar-agendamento/criar-agendamento.component';
 import { AgendamentoComponent } from './componentes/agendamentos/agendamento/agendamento.component';
 import { ListarAgendamentosComponent } from './componentes/agendamentos/listar-agendamentos/listar-agendamentos.component';
+import { ModalExcluirComponent } from './componentes/modal/modal-excluir/modal-excluir.component';
+import { ModalEditarComponent } from './componentes/modal/modal-editar/modal-editar.component';
+
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MomentDateModule } from '@angular/material-moment-adapter';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_FORMATS } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -22,18 +32,43 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ListarAgendamentosComponent,
     FooterComponent,
     NavBarComponent,
+    ModalEditarComponent,
+    ModalExcluirComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MomentDateModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatCardModule,
-    MatDatepickerModule,
     MatPaginatorModule,
-    BrowserAnimationsModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: {
+        parse: {
+          dateInput: ['l', 'LL'],
+        },
+        display: {
+          dateInput: 'L',
+          monthYearLabel: 'MMM YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'MMMM YYYY',
+        },
+      },
+    }
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+export class CustomDateModule {}
