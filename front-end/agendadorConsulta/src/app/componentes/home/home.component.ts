c04da80f0aacef3d91bdd730a18f92fe55ace292
Rafael Leite <rafaelrlcnta@gmail.com>
2023-09-27 22:37:51 -0300

    feat: finalizando a seção de cadastro no home

--- front-end/agendadorConsulta/src/app/componentes/home/home.component.ts
+++ front-end/agendadorConsulta/src/app/componentes/home/home.component.ts
@@ -25,50 +25,58 @@ export class HomeComponent implements OnInit {
       login: ['', Validators.required],
       password: ['', Validators.required],
       role: [1],
+      nomeCompleto: [''],
+      identidade: [''],
     });
   }
 
   registrar() {
+    this.tirarEspaçosVazios('login'), this.tirarEspaçosVazios('password');
+    console.log(this.registroForm.value);
     this.service.registrar(this.registroForm.value).subscribe({
-
       next: (response) => {
-        console.log(response);
         if (response.statusText === 'OK') {
           this._snackBar.open('Registro realizado com sucesso', 'Fechar', {
             duration: 5000,
             verticalPosition: 'top',
             panelClass: 'custom-snackbar',
           });
-
-          this.registroForm.removeControl('role');
-          this.service.login(this.registroForm.value).subscribe({
-            next: (response) => {
-              if (response?.user) {
-                this.router.navigate(['criarAgendamento']);
-                this._snackBar.open('Bem vindo!', 'Fechar', {
-                  duration: 5000,
-                  verticalPosition: 'top',
-                  panelClass: 'custom-snackbar',
-                });
-              }
-            },
-            error: () =>
-              this._snackBar.open('Ocorreu um erro no Login', 'Fechar', {
-                duration: 5000,
-                verticalPosition: 'top',
-                panelClass: 'custom-snackbar',
-              }),
-          });
         }
       },
-      error: (response) => {
-        console.log(response);
+      error: () => {
         this._snackBar.open('Ocorreu um erro no Registro', 'Fechar', {
           duration: 5000,
           verticalPosition: 'top',
           panelClass: 'custom-snackbar',
         });
       },
+      complete: () => {
+        this.registroForm.removeControl('role');
+        this.service.login(this.registroForm.value).subscribe({
+          next: (response) => {
+            if (response?.user) {
+              this.router.navigate(['criarAgendamento']);
+              this._snackBar.open('Bem vindo!', 'Fechar', {
+                duration: 5000,
+                verticalPosition: 'top',
+                panelClass: 'custom-snackbar',
+              });
+            }
+          },
+          error: () =>
+            this._snackBar.open('Ocorreu um erro no Login', 'Fechar', {
+              duration: 5000,
+              verticalPosition: 'top',
+              panelClass: 'custom-snackbar',
+            }),
+        });
+      },
     });
+  }
+
+  private tirarEspaçosVazios(campo: string) {
+    const campoControl = this.registroForm.get(campo)?.value;
+    const campoControlSemEspacos = campoControl.replace(' ', '').trim();
+    this.registroForm.get(campo)?.setValue(campoControlSemEspacos);
   }
 }
